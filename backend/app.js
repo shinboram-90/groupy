7f692378d6039ab8d694ed6fdc243857516ef811
Lisa Chatellier <78473267+shinboram-90@users.noreply.github.com>
2022-01-04 23:20:49 +0100

    mysql2 and DB created

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,37 @@
 const express = require("express");
+// Path module has many useful properties and methods to access and manipulate paths in the file system.
+const path = require("path");
 const helmet = require("helmet");
+const cors = require("cors");
+const morgan = require("morgan");
+const rateLimit = require("express-rate-limit");
+const mysql = require("mysql2");
+require("dotenv").config();
+
+const connection = mysql.createConnection({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASS,
+  database: process.env.MYSQL_DB,
+});
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+});
+
+connection.connect(function (err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("connection created with Mysql successfully");
+  }
+});
 
 const app = express();
 
 app.use(helmet());
+app.use(cors());
 
 app.use((req, res, next) => {
   console.log("Requête reçue !");
@@ -16,12 +44,17 @@ app.use((req, res, next) => {
 });
 
 app.use((req, res, next) => {
-  res.json({ message: "Votre requête a bien été reçue !" });
+  res.json({ message: "All good mate!" });
   next();
 });
 
-app.use((req, res, next) => {
-  console.log("Réponse envoyée avec succès !");
-});
+app.use(express.json());
+
+app.use("/images", express.static(path.join(__dirname, "images")));
+
+// Renvoie les requetes dans la console
+app.use(morgan("tiny"));
+
+app.use(limiter);
 
 module.exports = app;
