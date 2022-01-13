95ed05318b0794291c7899227ea1f1cbdb5d08fb
Lisa Chatellier <78473267+shinboram-90@users.noreply.github.com>
2022-01-13 14:56:57 +0100

    starting with user

---
+++ backend/routes/post.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router = express.Router();
+
+const auth = require("../middleware/auth");
+const multer = require("../middleware/multer-config");
+
+const postCtrl = require("../controllers/post");
+
+router.get("/", auth, postCtrl.getAllPosts);
+router.get("/:id", auth, postCtrl.getOnePost);
+
+router.post("/", auth, multer, postCtrl.createPost);
+router.put("/:id", auth, multer, postCtrl.modifyPost);
+router.delete("/:id", auth, postCtrl.deletePost);
+router.post("/:id/like", auth, postCtrl.likeSauce);
+
+// router.put("/:id/admin", auth, multer, postCtrl.modifyOnepostAdmin);
+// router.put("/admin", auth, multer, postCtrl.modifyAllpostsAdmin);
+
+module.exports = router;
---
+++ backend/routes/user.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+
+const auth = require("../middleware/auth");
+const multer = require("../middleware/multer-config");
+
+const userCtrl = require("../controllers/user");
+
+router.post("/signup", userCtrl.signup);
+router.post("/login", userCtrl.login);
+
+// router.get("/", auth, userCtrl.getAllUsers);
+// router.get("/:id", auth, userCtrl.getOneUser);
+
+// router.put("/:id", auth, multer, userCtrl.modifyUser);
+// router.delete("/:id", auth, userCtrl.deleteUser);
+
+// router.put("/:id/admin", auth, multer, userCtrl.modifyOneUserAdmin);
+// router.put("/admin", auth, multer, userCtrl.modifyAllUsersAdmin);
+
+module.exports = router;
