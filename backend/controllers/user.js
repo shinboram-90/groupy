509397cb08b86659cac8fa340073b1e8f921924e
Lisa Chatellier <78473267+shinboram-90@users.noreply.github.com>
2022-01-18 23:18:21 +0100

    can connect but still not done

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,6 +1,6 @@
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
-const db = require("../dbConnection");
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const db = require('../dbConnection');
 // const User = require("../models/User");
 // const router = require("../routes/user");
 
@@ -12,7 +12,7 @@ exports.signup = (req, res, next) => {
     (err, result) => {
       if (result.length) {
         return res.status(409).send({
-          msg: "This user is already in use!",
+          msg: 'This user is already in use!',
         });
       } else {
         // username is available
@@ -35,7 +35,7 @@ exports.signup = (req, res, next) => {
                   });
                 }
                 return res.status(201).send({
-                  msg: "The user has been registerd with us!",
+                  msg: 'The user has been registerd with us!',
                 });
               }
             );
@@ -48,51 +48,51 @@ exports.signup = (req, res, next) => {
 
 exports.login = (req, res, next) => {
   db.query(
-    `SELECT * FROM users WHERE email = ${db.escape(req.body.email)};`,
-    (err, result) => {
-      // user does not exists
-      if (err) {
-        return res.status(400).send({
-          msg: err,
-        });
-      }
-      if (!result.length) {
-        return res.status(401).send({
-          msg: "Email or password is incorrect!",
-        });
-      }
-      // check password
-      bcrypt.compare(
-        req.body.password,
-        result[0]["password"],
-        (bErr, bResult) => {
-          // wrong password
-          if (bErr) {
-            return res.status(401).send({
-              msg: "Email or password is incorrect!",
-            });
-          }
-          if (bResult) {
-            const token = jwt.sign(
-              { id: result[0].id },
-              "the-super-strong-secrect",
-              { expiresIn: "1h" }
-            );
-            db.query(
-              `UPDATE users SET last_login = now() WHERE id = '${result[0].id}'`
-            );
-            return res.status(200).send({
-              msg: "Logged in!",
-              token,
-              user: result[0],
-            });
-          }
-          return res.status(401).send({
-            msg: "Username or password is incorrect!",
-          });
-        }
-      );
-    }
+    `SELECT * FROM users`
+    // (err, result) => {
+    //   // user does not exists
+    //   if (err) {
+    //     return res.status(400).send({
+    //       msg: err,
+    //     });
+    //   }
+    // if (!result.length) {
+    //   return res.status(401).send({
+    //     msg: "Email or password is incorrect!",
+    //   });
+    // }
+    // check password
+    //   bcrypt.compare(
+    //     req.body.password,
+    //     result[0]["password"],
+    //     (bErr, bResult) => {
+    //       // wrong password
+    //       if (bErr) {
+    //         return res.status(401).send({
+    //           msg: "Email or password is incorrect!",
+    //         });
+    //       }
+    //       if (bResult) {
+    //         const token = jwt.sign(
+    //           { id: result[0].id },
+    //           "the-super-strong-secrect",
+    //           { expiresIn: "1h" }
+    //         );
+    //         db.query(
+    //           `UPDATE users SET last_login = now() WHERE id = '${result[0].id}'`
+    //         );
+    //         return res.status(200).send({
+    //           msg: "Logged in!",
+    //           token,
+    //           user: result[0],
+    //         });
+    //       }
+    //       return res.status(401).send({
+    //         msg: "Username or password is incorrect!",
+    //       });
+    //     }
+    //   );
+    // }
   );
 };
 
--- backend/dbConnection.js
+++ backend/dbConnection.js
@@ -1,17 +1,29 @@
-const mysql = require("mysql2");
+const mysql = require('mysql');
 
 const connection = mysql.createConnection({
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASS,
-  database: process.env.MYSQL_DB,
+  // unix_socket: '/Applications/MAMP/Library/bin/mysql',
+  // /Applications/MAMP/Library/bin/mysqld --skip-grant-tables
+  // /Applications/MAMP/Library/bin/mysql --host=localhost -uroot -proot
+  port: 8889,
+  password: 'root',
+  database: 'groupomania',
 });
 
 connection.connect(function (err) {
   if (err) {
     console.log(err);
   } else {
-    console.log("connection created with Mysql successfully");
+    console.log('connection created with Mysql 1 successfully');
+  }
+});
+
+connection.query('SELECT * FROM users', (err, rows) => {
+  if (!err) {
+    console.log(rows);
+  } else {
+    console.log(err);
   }
 });
 
