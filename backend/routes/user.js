a18fa0b50046bd00239fd7047c0070e17de5b393
Lisa Chatellier <78473267+shinboram-90@users.noreply.github.com>
2022-01-27 15:39:30 +0100

    user validations done

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const router = express.Router();
 
-// en attente de pouvoir recuperer tokens generes par le front ?
+const validator = require('../middleware/validator');
+
 // const auth = require('../middleware/auth');
 const multer = require('../middleware/multer-config');
 
 const userCtrl = require('../controllers/user');
 
-router.post('/signup', userCtrl.signup);
+router.post(
+  '/signup',
+  validator.checkBody,
+  validator.checkRules,
+  userCtrl.signup
+);
 // router.post('/users', userCtrl.createUser);
 router.delete('/users/:id', userCtrl.deleteUser);
 router.post('/login', userCtrl.login);
@@ -15,7 +21,12 @@ router.post('/login', userCtrl.login);
 router.get('/users', userCtrl.getAllUsers);
 router.get('/users/:id', userCtrl.getOneUser);
 
-router.put('/users/:id', userCtrl.updateUser);
+router.put(
+  '/users/:id',
+  validator.checkBody,
+  validator.checkRules,
+  userCtrl.updateUser
+);
 
 // router.put("/:id/admin", auth, multer, userCtrl.modifyOneUserAdmin);
 // router.put("/admin", auth, multer, userCtrl.modifyAllUsersAdmin);
